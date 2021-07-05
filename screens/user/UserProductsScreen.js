ee3022096347dffcdb40cd360cffb65ff42553a8
Rajeet <rajeetdps@gmail.com>
2021-07-05 22:23:51 +0530

    Reorganising product item(admin)

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {FlatList} from 'react-native';
+import {FlatList,Button,Platform} from 'react-native';
 import ProductItem from '../../components/shop/ProductItem';
 import { useSelector } from 'react-redux';
 import { HeaderButtons,Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
+import Colors from '../../constants/Colors';
 
 const UserProductsScreen = props=>{
     const userProducts = useSelector(state => state.products.userProducts);
@@ -16,9 +17,24 @@ const UserProductsScreen = props=>{
         image={itemData.item.imageUrl}
         title={itemData.item.title}
         price={itemData.item.price}
-        onViewDetail={()=>{}}
-        onAddToCart={()=>{}}
-        />)}
+        onSelect={()=>{}}
+        > 
+            <Button
+            color={Colors.btn}
+            title="Edit"
+            onPress={()=>{
+               
+             }}
+          />
+          <Button
+            color={Colors.btn}
+            title="Delete"
+            onPress={()=>{
+                
+            }}
+          />
+      
+       </ProductItem>)}
         />
     );
    
