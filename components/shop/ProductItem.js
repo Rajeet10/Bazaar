bf4c54ad48e8f4ba7196d5b1fb745f253644e129
Rajeet <rajeetdps@gmail.com>
2021-07-06 01:17:59 +0530

    Reusing component by adding card.js in UI-directory

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -4,12 +4,12 @@ import {
   View,
   Text,
   Image,
-  Button,
   TouchableOpacity,
   TouchableNativeFeedback,
   Platform,
 } from "react-native";
-import Colors from "../../constants/Colors";
+import Card from "../UI/Card";
+
 
 const ProductItem = (props) => {
     let TouchableCmp=TouchableOpacity;
@@ -17,7 +17,7 @@ const ProductItem = (props) => {
         TouchableCmp=TouchableNativeFeedback
     }
   return (
-      <View style={styles.product}>
+      <Card style={styles.product}>
           <View style={styles.touchable}>
           <TouchableCmp onPress={props.onSelect} useForeground>
               <View>
@@ -34,20 +34,13 @@ const ProductItem = (props) => {
         </View>
         </TouchableCmp>
         </View>
-      </View>
+      </Card>
   
   );
 };
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
     height: 300,
     margin: 20,
     
