b83513d982485b465b48aa077dd570346cc34dc7
Rajeet <rajeetdps@gmail.com>
2021-07-06 17:28:44 +0530

    Connecting Input comp to editproductscreen

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,12 +1,22 @@
-import React,{useReducer} from 'react';
+import React,{useReducer,useEffect} from 'react';
 import {Text,View,StyleSheet} from 'react-native';
 
 const INPUT_CHANGE='INPUT_CHANGE';
+const INPUT_BLUR='INPUT_BLUR';
 
 const inputReducer=(state,action)=>{
     switch(action.type){
         case INPUT_CHANGE:
-
+            return{
+                ...state,
+                value:action.value,
+                isValid:action.isValid
+            };
+        case INPUT_BLUR:
+            return{
+                ...state,
+                touched:true
+            };
     default:
         return state;
     }
@@ -14,10 +24,19 @@ const inputReducer=(state,action)=>{
 
 const Input = props=>{
     const [inputState,dispatch]=useReducer(inputReducer,{
-        value:props.intialValues ? props.intialValues : '',
+        value:props.intialValue ? props.intialValue : '',
          isValid:props.initiallyValid,
         touched:false
     });
+
+    const {onInputChange,id}=props;
+
+    useEffect(()=>{
+        if(inputState.touched){
+            onInputChange(id,inputState.value,inputState.isValid);
+        }
+       
+    },[inputState,onInputChange,id]);
 
 
     const textChangeChangeHandler=text=>{
@@ -41,21 +60,25 @@ const Input = props=>{
 
         dispatch({type:INPUT_CHANGE,value:text,isValid:isValid});
     };
+    const lostFocusHandler=()=>{
+        dispatch({type:INPUT_BLUR})
+    };
     return(
         <View style={styles.formControl}>
         <Text style={styles.label}>{props.label}</Text>
         <TextInput 
         {...props }
         style={styles.input} 
-        value={formState.inputValues.title} 
+        value={inputState.value} 
         onChangeText={textChangeChangeHandler}
+        onBlur={lostFocusHandler}
         />
-        {!formState.inputValidities.title && 
+        {!inputState.isValid && 
         <Text>{props.errorText}</Text>}
       </View>
     );  
     
-};
+}; 
 
 const styles=StyleSheet.create({
     formControl:{
