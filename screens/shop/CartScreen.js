adef3b9102c45d3c7134d9b47eda82df70f8b0f7
Rajeet <rajeetdps@gmail.com>
2021-07-07 20:03:55 +0530

    adding loader while storing into firebase

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+import React,{useState} from "react";
+import { View, Text, FlatList, StyleSheet, Button,ActivityIndicator } from "react-native";
 import { useSelector,useDispatch } from "react-redux";
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
@@ -9,6 +9,8 @@ import Card from "../../components/UI/Card";
 
 
 const CartScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
   const cartItems=useSelector(state=>{
       const transformedItems=[];
@@ -26,20 +28,29 @@ const CartScreen = (props) => {
       );
   });
   const dispatch=useDispatch();
+  const sendOrderHandler=async()=>{
+    setIsLoading(true);
+    await dispatch(ordersActions.addOrder(cartItems,cartTotalAmount));
+    setIsLoading(false);
+  }
 
   return (
     <View style={styles.screen}>
       <Card style={styles.summary}>
         <Text style={styles.summaryText}>Total:
-        <Text style={styles.amount }>${Math.round(cartTotalAmount.toFixed(2)*100)/100}</Text></Text>
+        <Text style={styles.amount }>${Math.round(cartTotalAmount.toFixed(2)*100)/100}</Text>
+        </Text>
+        {isLoading ? <ActivityIndicator
+        size="small" color={Colors.primary}/>
+        :
         <Button 
         color={Colors.accent} 
         title="Order Now"  
         disabled={cartItems.length===0}
-        onPress={()=>{
-          dispatch(ordersActions.addOrder(cartItems,cartTotalAmount));
-        }}
+        onPress={sendOrderHandler}
         />
+        }
+    
       </Card>
       <FlatList 
       data={cartItems}
